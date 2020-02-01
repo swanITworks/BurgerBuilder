15a5f131943f630a49a9b9b169dc11792c59b3ec
swanITworksl <michal@labi.pl>
2020-02-01 21:33:02 +0100

    added new dumb components - Burger and Ingredient

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Burger.module.css"
+import Aux from "../../hoc/Aux/Aux";
+
+const Burger = (props) => {
+    return (
+        <Aux>
+
+        </Aux>
+    )
+};
+
+export default Burger;
