293da905ba36a34ec0ce002a81929866ad9c5b6f
swanITworksl <michal@labi.pl>
2020-02-01 20:13:55 +0100

    base app structure

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Aux from "../../hoc/Aux/Aux";
+
+const layout = (props) => {
+    return (
+        <Aux>
+            <div>
+                Toolbar, SideDrawer,Backdrop
+            </div>
+            <main>
+                {props.children}
+            </main>
+        </Aux>
+    )
+};
+
+export default layout;
