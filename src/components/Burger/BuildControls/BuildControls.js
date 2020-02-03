c579eb6b86ceb29d928f2dfc1d2f92a2967301f9
swanITworksl <michal@labi.pl>
2020-02-03 10:31:20 +0100

    created own algorithm for burger builder igredients control menu

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,10 +3,10 @@ import classes from './BuildControls.module.css';
 
 const BuildControls = (props) => {
 
-    const ingredients = props.ingredients;
+    const {ingredients,changeIngredientsHandler} = props;
     const ingredientsArray = Object.entries(ingredients);
     const ingredientsControls = ingredientsArray.map(item=>(
-        <p key={item[0]}><span>{item[0]}</span> <span>-</span> <span>{item[1]}</span> <span>+</span></p>
+        <p key={item[0]}><span>{item[0]}</span> <span onClick={()=>changeIngredientsHandler(item[0],'subs')}>-</span> <span>{item[1]}</span> <span onClick={()=>changeIngredientsHandler(item[0],'add')}>+</span></p>
         )
     );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,14 +16,32 @@ class BurgerBuilder extends Component {
 
     render() {
 
-        const changeIngredientsHandler = (ingredient) => {
-this.setState()
+        const changeIngredientsHandler = (ingredient, operation) => {
+            let newState = [...Object.entries(this.state.ingredients)];
+            const foundElementToChange = (newState.find(item => item[0] === ingredient));
+
+            if (operation === 'add') {
+                foundElementToChange[1] += 1;
+            };
+
+            if (operation === 'subs') {
+                if (foundElementToChange[1] > 0) {
+                    foundElementToChange[1] -= 1;
+                }
+            };
+
+            let ingredients = {};
+            for (let i = 0; i < newState.length; i++) {
+                ingredients[newState[i][0]] = newState[i][1];
+            }
+            this.setState({ingredients});
         };
 
         return (
             <Aux>
-                <Burger ingredients={this.state.ingredients} />
-                <BuildControls ingredients={this.state.ingredients} />
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls ingredients={this.state.ingredients}
+                               changeIngredientsHandler={changeIngredientsHandler}/>
             </Aux>
         );
     }
