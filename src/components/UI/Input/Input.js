43ee10940847b87bfc307f2837406fcc0b60c045
swanITworksl <michal@labi.pl>
2020-02-27 09:35:46 +0100

    working on dynamic order inputform

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,18 +2,27 @@ import React from "react";
 import classes from "./Input.module.css"
 
 const input = (props) => {
-    const {label, inputType} = this.props;
+    const {label, value, elementType} = props;
     let inputElement = null;
 
-    switch (inputType) {
+    switch (elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props} />;
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={value}/>;
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props} />;
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={value}/>;
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props} />
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={value}/>
     }
 
     return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,66 @@ import classes from './ContactData.module.css';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        userName: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: '',
+        orderForm: {
+
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name',
+                },
+                value: '',
+            },
+
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street',
+                },
+                value: '',
+            },
+
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code',
+                },
+                value: '',
+            },
+
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country',
+                },
+                value: '',
+            },
+
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your - email',
+                },
+                value: '',
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: '',
+            },
         },
         loading: false,
     };
@@ -22,16 +74,7 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
 
-            customer: {
-                name: this.state.userName,
-                address: {
-                    street: 'Szybownikow 4',
-                    zipCode: '64-920',
-                    country: 'Poland',
-                },
-                email: this.state.email,
-            },
-            deliveryMethod: 'fastest',
+
             price: this.props.price,
         };
 
@@ -49,17 +92,34 @@ class ContactData extends Component {
 
     handlerInputChange = (event) => {
         this.setState({
-            [event.target.name]:event.target.value,
+            [event.target.name]: event.target.value,
         })
     };
 
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push(
+                {
+                    id: key,
+                    config: this.state.orderForm[key]
+                }
+            )
+        }
+        ;
+
         let form = (
             <form>
-                <input className={classes.Input} type='text' name='userName' placeholder='Your name' onChange={this.handlerInputChange}/>
-                <input className={classes.Input} type='email' name='email' placeholder='Your email'/>
-                <input className={classes.Input} type='text' name='street' placeholder='Street'/>
-                <input className={classes.Input} type='text' name='postalCode' placeholder='Postal Code'/>
+                <Input elementType='...' elementConfig='...' value='...'/>
+                {formElementsArray.map(formElement => (
+                        <Input
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.elementConfig}
+                        />
+                    )
+                )
+                }
                 <Button btnType='Success' onClick={this.orderHandler}>ORDER</Button>
                 <Button>CANCEL</Button>
             </form>
